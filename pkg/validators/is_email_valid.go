0d85bd2d295bdbbafa94ea480e92cb063ebe2c45
Gabriel <francogm77@hotmail.com>
2024-04-27 13:26:03 -0300

    [feat] Create email regex validation

--- pkg/validators/is_email_valid.go
+++ pkg/validators/is_email_valid.go
@@ -1,1 +1,11 @@
 package validators
+
+import "regexp"
+
+func IsEmailValid(email string) bool {
+	rxEmail := regexp.MustCompile(
+		`^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$`,
+	)
+
+	return len(email) > 3 || len(email) < 254 || rxEmail.MatchString(email)
+}
